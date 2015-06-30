5d059e13d007a49267891ed12a1f32e93e7b24c5
tale <giangnguyen.tale@gmail.com>
2015-06-30 15:20:42 +0700

    ! optimize for Android

--- rxflux/src/main/java/com/tale/rxflux/Store.java
+++ rxflux/src/main/java/com/tale/rxflux/Store.java
@@ -1,6 +1,7 @@
 package com.tale.rxflux;
 
 import rx.Observable;
+import rx.android.lifecycle.LifecycleEvent;
 import rx.subjects.PublishSubject;
 import rx.subjects.SerializedSubject;
 import rx.subjects.Subject;
@@ -12,7 +13,7 @@ public abstract class Store<T> {
 
     private static final Subject<Object, Object> CHANGES_SUBJECT = new SerializedSubject<>(PublishSubject.create());
 
-    public void registerWithDispatcher(Dispatcher dispatcher) {
+    public void registerWithDispatcher(Dispatcher dispatcher, Observable<LifecycleEvent> lifecycle) {
     }
 
     /**
@@ -24,7 +25,14 @@ public abstract class Store<T> {
         return (Observable<T>) CHANGES_SUBJECT.asObservable();
     }
 
+    /**
+     * Call to notify that there is change happend.
+     *
+     * @param t the object to be emitted.
+     */
     protected void emitChange(T t) {
-        CHANGES_SUBJECT.onNext(t);
+        if (CHANGES_SUBJECT.hasObservers()) {
+            CHANGES_SUBJECT.onNext(t);
+        }
     }
 }
